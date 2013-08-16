ec5fb462b773196019ac9ecb377898bb4787c522
Rishabh Garg <rishabhgarg1992@gmail.com>
2013-08-16 21:23:31 +0530

    Updated models.py to add Store and Store Admin

--- smartShop/storefront/models.py
+++ smartShop/storefront/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+
+class Store(models.Model):
+        store_id = models.CharField(max_length=255,primary_key=True)
+        store_name = models.CharField(max_length=255)
+        store_address = models.CharField(max_length=255)
+        store_phone = models.CharField(max_length=255)
+        store_website = models.CharField(max_length=255)
+        store_email = models.CharField(max_length=255)
+        store_hours = models.CharField(max_length=255)   
+        
+class StoreAdmin(models.Model):
+        admin_id=models.CharField(_('password'),max_length=255)
+        admin_email = models.CharField(max_length=255)
+        admin_password = models.CharField(max_length=255)
+        admin_storeid = models.ForeignKey(Store)
+
+
