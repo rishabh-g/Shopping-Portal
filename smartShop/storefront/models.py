8183c9a9b5e52f06ffab5e81ef4c7879c621ca28
Rishabh Garg <rishabhgarg1992@gmail.com>
2013-09-18 20:16:13 +0530

    Requirements file changed | Files not used deleted | Migration incosistency solved | Template file changed to current syntax of URL

--- smartShop/storefront/models.py
+++ smartShop/storefront/models.py
@@ -3,17 +3,17 @@ from django.db import models
 # Create your models here.
 
 class Store(models.Model):
-        store_name = models.CharField(max_length=255)
-        store_address = models.CharField(max_length=255)
-        store_phone = models.CharField(max_length=255)
-        store_website = models.CharField(max_length=255)
-        store_email = models.CharField(max_length=255)
-        store_hours = models.CharField(max_length=255)   
+    store_name = models.CharField(max_length=255)
+    store_address = models.CharField(max_length=255)
+    store_phone = models.CharField(max_length=255)
+    store_website = models.CharField(max_length=255)
+    store_email = models.CharField(max_length=255)
+    store_hours = models.CharField(max_length=255)   
         
 class StoreAdmin(models.Model):
-        admin_email = models.CharField(max_length=255)
-        admin_password = models.CharField(max_length=255)
-        admin_storeid = models.ForeignKey(Store)
+    admin_email = models.CharField(max_length=255)
+    admin_password = models.CharField(max_length=255)
+    admin_storeid = models.ForeignKey(Store)
 
 class ProductAlbum(models.Model):
     album_name = models.CharField(max_length=255)
@@ -21,10 +21,6 @@ class ProductAlbum(models.Model):
     album_product_number = models.IntegerField(max_length=255)
     album_photograph = models.CharField(max_length=255)
  
-
-
-
-
 class ProductDetails(models.Model):
     product_name = models.CharField(max_length=255)
     product_image = models.CharField(max_length=255)
