136c59f0d29af56e9dff1753e959e49b85c7910d
Rishabh Garg <rishabhgarg1992@gmail.com>
2013-09-20 20:49:54 +0530

    View updated for login and register | Model, API and other changes

--- smartShop/storefront/views.py
+++ smartShop/storefront/views.py
@@ -3,55 +3,53 @@ from django.http import HttpResponse
 from django.template import Context, loader
 from django.http import Http404,HttpResponseRedirect, HttpResponse
 from django.core.urlresolvers import reverse
-from django.shortcuts import get_object_or_404,render
+from django.shortcuts import get_object_or_404,render,redirect
 from storefront.models import Store,StoreAdmin 
 
 
 def home(request):
 #	return HttpResponse("U have been redirected to the home page - Hello World!")
-    if 'email' in request.session:
-        return render(request,'storefront/login.html')
-    else:
-        return render(request,'storefront/login.html')
+
+    return render(request,'storefront/login.html')
 
 def login(request):
-    print str(request.POST)
-    email = request.POST['email']
-    password = request.POST['password']
-    print type(email)
-    print "\n"
-    if(not email  or not password ): 
-        return HttpResponse("Enter the Details")
-    try:
-         userDetails = StoreAdmin.objects.get(admin_email = email)
-         print str(userDetails)
-    except StoreAdmin.DoesNotExist:
-        return HttpResponse("User doesnot exist")
-    print str(userDetails)
-    if userDetails.admin_password == password:
-        request.session['uid'] = userDetails.id
-        return HttpResponse("Login Successful")
 
-    return HttpResponse("Enter the Details")
+    if request.method=='POST':
+        email = request.POST['email']
+        password = request.POST['password']
+        if not email  or not password: 
+            return HttpResponse("Please enter the Details")
+        try:
+             userDetails = StoreAdmin.objects.get(admin_email = email)
+        except StoreAdmin.DoesNotExist:
+            return HttpResponse("User doesnot exist")
+        if userDetails.admin_password == password:
+            #request.session['uid'] = userDetails.id
+            return redirect('/api/v1/store/1/?format=json')
+
+        return HttpResponse("Enter the Details")
+    return render(request,'storefront/login.html')
+
 	
 
 def register(request):
-    store_name=request.POST['store_name']
-    store_address=request.POST['store_address']
-    store_phone=request.POST['store_phone']
-    store_website=request.POST['store_website']
-    store_email=request.POST['store_email']
-    store_hours=request.POST['store_hours']
+    if request.method=='POST':        
+        store_name=request.POST['store_name']
+        store_address=request.POST['store_address']
+        store_phone=request.POST['store_phone']
+        store_website=request.POST['store_website']
+        store_email=request.POST['store_email']
+        store_hours=request.POST['store_hours']
 
-    store=Store(store_name=store_name,store_address=store_address,store_phone=store_phone,store_website=store_website,store_email=store_email,store_hours=store_hours)
-    store.save()
-    admin_email=request.POST['admin_email']
-    admin_password=request.POST['admin_password']
-    admin_storeid=store
-    storead=StoreAdmin(admin_email=admin_email,admin_password=admin_password,admin_storeid=admin_storeid)
-    storead.save()
-    request.session['email']=admin_email
-    return home(request)
+        store=Store(store_name=store_name,store_address=store_address,store_phone=store_phone,store_website=store_website,store_email=store_email,store_hours=store_hours)
+        store.save()
+        admin_email=request.POST['admin_email']
+        admin_password=request.POST['admin_password']
+        admin_storeid=store
+        storead=StoreAdmin(admin_email=admin_email,admin_password=admin_password,admin_storeid=admin_storeid)
+        storead.save()
+        request.session['email']=admin_email
+    return render(request,'storefront/register.html')
 	
 
 def index(request):
